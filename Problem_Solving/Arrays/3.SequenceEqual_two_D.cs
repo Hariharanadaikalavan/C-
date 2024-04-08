c90d9a3c78d1b61a54b15a59e1aa94d48c356f70
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-04-08 15:15:51 +0530

    Create 3.SequenceEqual_two_D.cs

---
+++ Problem_Solving/Arrays/3.SequenceEqual_two_D.cs
@@ -0,0 +1,16 @@
+public class TwoDimensions{
+  public void Castmethod(){
+    int [,] arr={{1,2,3,4,5,6,7,8},{9,10,11,12,13,14,15,16}};
+    int [,] arr2={{1,2,3,4,5,6,7,8},{9,10,11,12,13,14,15,16}};
+
+    var first=arr.cast<int>();
+    var second=arr.cast<int>();
+
+    string result=first.SequenceEqual(second);
+    Console.readLine(result);
+  }
+  static void main(String args[]){
+    TwoDimensions obj=new TwoDimensions();
+    obj.
+  }
+}
